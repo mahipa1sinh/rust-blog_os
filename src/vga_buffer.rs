f7326cc779cabdadbdda623c32f8b459d41e8d8e
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-05-26 21:05:07 +0530

    use a spinlock mutex to allow multiple threads to access the VGA buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,6 @@
 use core::fmt::{Result, Write};
+use lazy_static::lazy_static;
+use spin::Mutex;
 use volatile::Volatile;
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -51,6 +53,14 @@ pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
 }
 
 impl Writer {
@@ -113,16 +123,4 @@ impl Write for Writer {
         self.write_string(s);
         Ok(())
     }
-}
-
-pub fn print_something() {
-    let mut writer: Writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
 }
