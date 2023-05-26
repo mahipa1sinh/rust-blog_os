b855d92df30629ad3c13d7da24d1aead14c4d76f
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-05-26 22:00:21 +0530

    added testable traits

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -42,27 +57,25 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() {
-    println!("Hello World{}", "!");
+    // println!("Hello World{}", "!");
 
     #[cfg(test)]
     test_main();
 
+    #[allow(clippy::empty_loop)]
     loop {}
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
-
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
