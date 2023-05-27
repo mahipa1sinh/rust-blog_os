19e0a08758944629a089036c43a63b5099e4eb8a
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-05-27 17:17:38 +0530

    using crate provided memory translations

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,13 @@
-use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
+use x86_64::{
+    structures::paging::OffsetPageTable, structures::paging::PageTable, PhysAddr, VirtAddr,
+};
 
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
